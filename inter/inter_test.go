61b3f8bcaf4d546f442153d2a35b02eb42792b30
tyler <linuxandchill@users.noreply.github.com>
2018-09-03 20:10:21 -0700

    Update inter_test.go

--- inter/inter_test.go
+++ inter/inter_test.go
@@ -13,11 +13,23 @@ func TestPerim(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	wanted := 100.0
 
-	if got != wanted {
-		t.Errorf("got %.2f, wanted %.2f", got, wanted)
+	checkArea := func(t *testing.T, shape Shape, wanted float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != wanted {
+			t.Errorf("got %.2f, wanted %.2f", got, wanted)
+		}
 	}
+
+	t.Run("area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+
+	t.Run("area of a circle", func(t *testing.T) {
+		circle := Circle{10.0} // pass in radius
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
