a02fd085225c885fc7746278635466f83239d5c2
tyler <linuxandchill@users.noreply.github.com>
2018-09-04 21:00:56 -0700

    Update inter_test.go

--- inter/inter_test.go
+++ inter/inter_test.go
@@ -14,6 +14,27 @@ func TestPerim(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 10.0}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.hasArea {
+				t.Errorf("got %.2f wanted %.2f", got, tt.hasArea)
+			}
+
+		})
+	}
+
 	checkArea := func(t *testing.T, shape Shape, wanted float64) {
 		t.Helper()
 		got := shape.Area()
