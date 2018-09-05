710aee44bf8adf833d1ef2ab1bcc9a51303e2818
tyler <linuxandchill@users.noreply.github.com>
2018-09-04 21:01:22 -0700

    Update inter.go

--- inter/inter.go
+++ inter/inter.go
@@ -11,12 +11,21 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
 
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Height + r.Width)
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
 }
 
 func (r Rectangle) Area() float64 {
